236ba26360a44dca137e968546f541366ee5dc09
rachelholmquist <Rachelflorek18@outlook.com>
2023-06-29 23:42:48 -0700

    almost done

--- src/Components/OptionDisplay.js
+++ src/Components/OptionDisplay.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { useSelector, useDispatch } from 'react-redux';
+import { selectPotentials } from '../redux/slices/potentialCountriesSlice';
+import { setDisplayCountry } from "../redux/slices/displayCountrySlice";
 
 const OptionDisplay = () => {
-    return <div className="stack">OptionDisplay</div>;
+    let currentPotentials = useSelector(selectPotentials);
+    console.log(currentPotentials);
+    const dispatch = useDispatch();
+
+    return <div className="stack">
+        {currentPotentials.map((e, i) => {
+            return (
+                <h2 key={e.name.official} className='country-option' onClick={() => {
+                    dispatch(setDisplayCountry(currentPotentials[i]));
+                }}>
+                    {e.name.common}
+                </h2>
+            );
+        })}
+            </div>;
 };
 
 export default OptionDisplay;
